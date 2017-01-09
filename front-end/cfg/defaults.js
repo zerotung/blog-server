76ff2ba7719bdd02ce3739b60092c0206074f13c
Zero Tung <zero_tung@163.com>
2017-01-09 16:39:49 +0800

    添加了博客的navbar 1. 修改样式允许使用sass 2. 添加了postcss-loader等 3. 添加了blog的navbar TODO: 添加首页

--- front-end/cfg/defaults.js
+++ front-end/cfg/defaults.js
@@ -8,7 +8,7 @@
 
 const path = require('path');
 const srcPath = path.join(__dirname, '/../src');
-const dfltPort = 8000;
+const dfltPort = 8001;
 
 /**
  * Get the default modules object for webpack
@@ -16,43 +16,33 @@ const dfltPort = 8000;
  */
 function getDefaultModules() {
   return {
-    preLoaders: [
-      {
-        test: /\.(js|jsx)$/,
-        include: srcPath,
-        loader: 'eslint-loader'
-      }
-    ],
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-      {
-        test: /\.sass/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
-      },
-      {
-        test: /\.scss/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
-      },
-      {
-        test: /\.less/,
-        loader: 'style-loader!css-loader!less-loader'
-      },
-      {
-        test: /\.styl/,
-        loader: 'style-loader!css-loader!stylus-loader'
-      },
-      {
-        test: /\.(png|jpg|gif|woff|woff2)$/,
-        loader: 'url-loader?limit=8192'
-      },
-      {
-        test: /\.(mp4|ogg|svg)$/,
-        loader: 'file-loader'
-      }
-    ]
+    preLoaders: [{
+      test: /\.(js|jsx)$/,
+      include: srcPath,
+      loader: 'eslint-loader'
+    }],
+    loaders: [{
+      test: /\.css$/,
+      loader: 'style-loader!css-loader!postcss-loader'
+    }, {
+      test: /\.sass/,
+      loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+    }, {
+      test: /\.scss/,
+      loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded'
+    }, {
+      test: /\.less/,
+      loader: 'style-loader!css-loader!less-loader'
+    }, {
+      test: /\.styl/,
+      loader: 'style-loader!css-loader!stylus-loader'
+    }, {
+      test: /\.(png|jpg|gif|woff|woff2)$/,
+      loader: 'url-loader?limit=8192'
+    }, {
+      test: /\.(mp4|ogg|svg)$/,
+      loader: 'file-loader'
+    }]
   };
 }
 
--- front-end/src/components/Main.js
+++ front-end/src/components/Main.js
@@ -1,22 +1,82 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+class Navbar extends React.Component {
 
-class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
-  }
+    render() {
+
+        let navList = [];
+
+        this.props.data.forEach((data) => {
+
+            let liClassName = '';
+            liClassName += data.isActive ? 'active' : '';
+            navList.push(<li key={data.name} className={liClassName}><a>{data.title}</a></li>)
+        })
+
+        return (
+            <nav className="navbar">
+                <ul className="nav">
+                    {navList}
+                </ul>
+            </nav>
+        );
+    }
 }
 
-AppComponent.defaultProps = {
-};
+class AppComponent extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            navData: [{
+                name: 'home',
+                title: '首页',
+                isActive: true
+            }, {
+                name: 'article',
+                title: '我的推送',
+                isActive: false
+            }, {
+                name: 'display',
+                title: '作品展示',
+                isActive: false
+            }, {
+                name: 'aboutme',
+                title: '关于我',
+                isActive: false
+            }]
+        };
+    }
+
+    render() {
+
+        let navData = this.state.navData;
+
+        return (
+
+            <Navbar data={navData}/>
+        )
+    }
+
+    _renderPage() {
+        if (this.state.nav = 'home') {
+            return <HomePage />;
+        }
+        if (this.state.nav = 'aboutme') {
+            return <AboutmePage />;
+        }
+        if (this.state.nav = 'display') {
+            return <DisplayPage />;
+        }
+        if (this.state.nav = 'article') {
+            return <ArticlePage />;
+        }
+    }
+}
+
+AppComponent.defaultProps = {};
 
 export default AppComponent;
